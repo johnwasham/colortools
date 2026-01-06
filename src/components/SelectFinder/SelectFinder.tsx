e5a1e38963c8fb256e488ec5b88d45ccc897bb1b
John Washam <code@readyprompt.com>
2026-01-06 11:57:42 -0800

    Centered UI

--- src/components/SelectFinder/SelectFinder.tsx
+++ src/components/SelectFinder/SelectFinder.tsx
@@ -28,7 +28,7 @@ export const SelectFinder = () => {
 
     return (
         <>
-            <div>
+            <div className="selectFinder">
                 <h2>Select Blending Mode Color Finder</h2>
                 <p>
                     <strong>How to Use It:</strong><br />
